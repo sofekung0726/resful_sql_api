3ba99c007b329be97d94773766aa39e21db47d99
sofekung0726 <sofekung@gmail.com>
2023-07-19 10:09:38 +0700

    connect sequalize complete

--- models/db.js
+++ models/db.js
@@ -1,16 +1,24 @@
-const mysql = require('mysql');
-const dbConfig = require("../config/db.config");
+const {Sequelize} = require("sequelize");
+const dbConfig = require("../config/db.config")
 
-const connection = mysql.createConnection({
-    host: dbConfig.HOST,
-    user:dbConfig.USER,
-    password:dbConfig.PASSWORD,
-    database:dbConfig.DB
-});
-    connection.connect(
-    (error)=>{
-        if(error) throw error;
-        console.log("Succesfully connected to the database....");
-    })
+//create seq instance like clone 
 
-    module.exports = connection;
+const sequelize = new Sequelize(dbConfig.DB,dbConfig.USER,dbConfig.PASSWORD,{
+    host:dbConfig.HOST,
+    dialect:"mysql"
+
+})
+
+// test database connection
+async function testConnection(){
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+}
+testConnection();
+module.exports = sequelize;
+
+
